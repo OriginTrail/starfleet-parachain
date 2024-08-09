a0cd99e58f62070debfce9f817e8bb0ec5ddd334
NZT48 <nikolaztodorovic26@gmail.com>
2024-08-09 14:54:59 +0200

    Revert milisec per block

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -163,7 +163,7 @@ pub const RELAY_CHAIN_SLOT_DURATION_MILLIS: u32 = 6000;
 /// up by `pallet_aura` to implement `fn slot_duration()`.
 ///
 /// Change this to adjust the block time.
-pub const MILLISECS_PER_BLOCK: u64 = 6000;
+pub const MILLISECS_PER_BLOCK: u64 = 12000;
 
 // NOTE: Currently it is not possible to change the slot duration after the chain has started.
 //       Attempting to do so will brick block production.
